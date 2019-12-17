e4d346a99ef7d6c0eadfcece60b0bfae7a87c0c4
Joaquin Caceres <jncaceres@uc.cl>
2019-12-17 18:33:58 -0300

    fixes to charts

---
+++ Huihuinga/Models/ConcreteFeedback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Huihuinga.Models
+{
+    public class ConcreteFeedback
+    {
+        public double Quality { get; set; }
+        public string Label { get; set; }
+
+    }
+}
